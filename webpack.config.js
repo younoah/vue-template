5693b80ff1fc5fb0d5b5dbbf5dca38dfccd1c286
younoah <kr_uno_10@naver.com>
2021-10-10 22:36:04 +0900

    npm: babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
         test: /\.vue$/,
         use: 'vue-loader',
       },

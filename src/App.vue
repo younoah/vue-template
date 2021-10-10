e8dcde4415e6a74c44724fade445f8ff3c2fddea
younoah <kr_uno_10@naver.com>
2021-10-10 14:13:11 +0900

    init: 뷰, scss, html, copy 플러그인

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello';
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: green;
+}
+</style>
